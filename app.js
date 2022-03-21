25ad8e416b328356c0b1b7927ff9bdfc5e3f0293
baldevpardeshi <baldevpardeshi1@gmail.com>
2022-03-21 12:59:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAMFjnBG-EtzGH8Eaj2oXYOOwsfVWDh4YQ",
+  authDomain: "bramhacorp-leads.firebaseapp.com",
+  databaseURL: "https://bramhacorp-leads.firebaseio.com",
+  projectId: "bramhacorp-leads",
+  storageBucket: "bramhacorp-leads.appspot.com",
+  messagingSenderId: "924127093335",
+  appId: "1:924127093335:web:826c40ff6b26f559447912",
+  measurementId: "G-HWN125WV8G",
+};
+firebase.initializeApp(firebaseConfig);
---
+++ forms.js
@@ -0,0 +1,175 @@
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return true;
+  }
+  return false;
+}
+
+function isValidForm(data) {
+  return (
+    data.name && ValidateEmail(data.email) && data.phoneNumber && data.city
+  );
+}
+
+function postForm(data) {
+  window.firebase
+    .database()
+    .ref("Commercial Project")
+    .push()
+    .set(data)
+    .then((res) => {
+      window.location.replace(
+        "https://campaign.bramhacorp.in/business-park/thank-you.html"
+      );
+    });
+}
+
+function getParameter() {
+  const address = window.location.search;
+  const parameterList = new URLSearchParams(address);
+  return {
+    utmSource: parameterList.get("utm_source") || null,
+    utmCampaign: parameterList.get("utm_campaign") || null,
+    utmMedium: parameterList.get("utm_medium") || null,
+  };
+}
+
+function submitEnquiry() {
+  const pageURL = window.location.href;
+  const data = {
+    pageURL,
+    name: document.getElementById("name01").value,
+    email: document.getElementById("email01").value,
+    phoneNumber: document.getElementById("phone01").value,
+    city: document.getElementById("city01").value,
+    message: document.getElementById("msg01").value,
+    createdDt: new Date().toString(),
+    projectName: "Business Park (Landing page)",
+    ...getParameter(),
+  };
+  if (isValidForm(data)) {
+    window.dataLayer.push({
+      event: "Submit Button Click",
+      products: [
+        {
+          pageURL,
+        },
+      ],
+    });
+
+    window.dataLayer.push({
+      event: "Lead Submitted",
+      products: [{
+        NAME: document.getElementById("name01").value,
+        email: document.getElementById("email01").value,
+        phone: document.getElementById("phone01").value,
+        CITY: document.getElementById("city01").value,
+        message: document.getElementById("msg01").value,
+        createdDt: new Date().toString(),
+        projectName: "Business Park (Landing page)",
+      }],
+    });
+
+    postForm(data);
+  }
+}
+
+function submitEnquiryFooter() {
+  const pageURL = window.location.href;
+  const data = {
+    pageURL,
+    name: document.getElementById("name03").value,
+    email: document.getElementById("email03").value,
+    phoneNumber: document.getElementById("phone03").value,
+    city: document.getElementById("city03").value,
+    message: document.getElementById("msg03").value,
+    createdDt: new Date().toString(),
+    projectName: "Business Park (Landing page)",
+    ...getParameter(),
+  };
+  if (isValidForm(data)) {
+    window.dataLayer.push({
+      event: "Submit Button Click",
+      products: [
+        {
+          pageURL,
+        },
+      ],
+    });
+
+    window.dataLayer.push({
+      event: "Lead Submitted",
+      products: [{
+        NAME: document.getElementById("name03").value,
+        email: document.getElementById("email03").value,
+        Phone: document.getElementById("phone03").value,
+        CITY: document.getElementById("city03").value,
+        message: document.getElementById("msg03").value,
+        createdDt: new Date().toString(),
+        projectName: "Business Park (Landing page)",
+      }],
+    });
+    postForm(data);
+  }
+}
+
+function submitFloorPlans() {
+  const pageURL = window.location.href;
+  const data = {
+    pageURL,
+    name: document.getElementById("name02").value,
+    email: document.getElementById("email02").value,
+    phoneNumber: document.getElementById("phone02").value,
+    message: document.getElementById("msg02").value,
+    city: " ",
+    createdDt: new Date().toString(),
+    projectName: "Business Park (Landing page)",
+    ...getParameter(),
+  };
+  if (isValidForm(data)) {
+    window.dataLayer.push({
+      event: "Submit Button Click",
+      products: [
+        {
+          pageURL,
+        },
+      ],
+    });
+
+    window.dataLayer.push({
+      event: "Lead Submitted",
+      products: [{
+        NAME: document.getElementById("name02").value,
+        email: document.getElementById("email02").value,
+        Phone: document.getElementById("phone02").value,
+        message: document.getElementById("msg02").value,
+        CITY: " ",
+        createdDt: new Date().toString(),
+        projectName: "Business Park (Landing page)",
+      }],
+    });
+
+    postForm(data);
+  }
+}
+
+function onClickWhatsapp () {
+  window.dataLayer.push({
+    event: "Whatsapp Button Click",
+    products: [
+      {
+        pageURL,
+      },
+    ],
+  });
+}
+function onClickCall () {
+  window.dataLayer.push({
+    event : 'Call Button Click',
+    products: [
+      {
+        pageURL,
+      },
+    ],
+  });
+}
